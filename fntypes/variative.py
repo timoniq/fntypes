e16db362e8be1a656afa01300ae68767318b3ad5
Georgy Pliev <howluwqz1@gmail.com>
2024-02-14 17:52:41 +0300

    fix: unpack typing for Variative (#1)

--- fntypes/variative.py
+++ fntypes/variative.py
@@ -2,50 +2,51 @@ from __future__ import annotations
 
 import typing
 
+from fntypes.result import Error, Ok, Result
 from fntypes.tools import RuntimeGeneric
-from fntypes.result import Result, Ok, Error
 
-Ts = typing.TypeVarTuple("Ts")  # does not support bounds :(
-Ps = typing.TypeVarTuple("Ps")
 T = typing.TypeVar("T")
 P = typing.TypeVar("P")
+Ts = typing.TypeVarTuple("Ts")
 
-_ = typing.NewType("_", type)
 HEAD = typing.NewType("HEAD", type)
 
-class Variative(RuntimeGeneric, typing.Generic[typing.Unpack[Ts]]):
 
-    def __init__(self, value: typing.Union[typing.Unpack[Ts]]):
-       self.value = value
+class Variative(RuntimeGeneric, typing.Generic[*Ts]):
+    def __init__(self: Variative[*tuple[T, ...]], value: T) -> None:
+        self._value: typing.Any = value
 
     def __repr__(self) -> str:
-        args = self.get_args()
-        return "Variative[{}]({})".format(", ".join([arg.__name__ for arg in args]), self.value)
+        return "Variative[{}]({!r})".format(
+            ", ".join(arg.__name__ if isinstance(arg, type) else repr(arg) for arg in self.get_args()),
+            self._value,
+        )
     
     @property
-    def v(self) -> typing.Union[typing.Unpack[Ts]]:
-        """Junction; intersection of Variative types"""
-        return self.value
-    
-    @typing.overload
-    @classmethod
-    def get_args(cls) -> tuple[type, ...]:
-        """overload Proxy.get_args"""
+    def v(self: Variative[*tuple[T, ...]]) -> T:
+        """Junction; intersection of Variative types."""
+
+        return self._value
 
     @typing.overload
-    def get_args(self) -> tuple[type, ...]:
-        ...
-    
-    def get_args(self):  # type: ignore
-       return typing.get_args(self.__orig_class__)  # type: ignore
+    def get_args(self) -> tuple[*Ts]:  # type: ignore
+        """Overload `Variative.get_args`"""
+
+    @typing.overload
+    @classmethod
+    def get_args(cls) -> tuple[*Ts]:
+        """Overload `Proxy.get_args`"""
         
+    def get_args(self) -> tuple[*Ts]:  # type: ignore
+        return typing.get_args(self.__orig_class__)  # type: ignore
+
     @typing.overload
     def only(self, t: type[T]) -> Result[T, str]:
         # Probably there is not way for a better typing until T cannot be bound to Ts or intersection typehint is implemented
         ...
 
     @typing.overload
-    def only(self: "Variative[T, typing.Unpack[Ps]]", t = HEAD) -> Result[T, str]:
+    def only(self: Variative[T, *tuple[P, ...]], t = HEAD) -> Result[T, str]:
         ...
     
     @typing.overload
@@ -54,10 +55,10 @@ class Variative(RuntimeGeneric, typing.Generic[typing.Unpack[Ts]]):
         ...
 
     def only(
-        self: "Variative[T, typing.Unpack[Ps]]", 
+        self: Variative[T, *tuple[T, ...]], 
         t: type = HEAD,
-    ) -> Result[typing.Union[T, typing.Unpack[Ps]], str]:
-        """sets Variative to single type. By default this type is generic leading type
+    ) -> Result[T, str]:
+        """Sets `Variative` to single type. By default this type is generic leading type
         ```python
         u: Variative[str, int] = Variative("Hello")
         u.only() # Ok("Hello")
@@ -66,53 +67,42 @@ class Variative(RuntimeGeneric, typing.Generic[typing.Unpack[Ts]]):
         u.only(list) # Err
         ```
         """
+
         if t == HEAD:
-            t = self.get_args()[0]
-        if not isinstance(self.value, t):
+            t = self.get_args()[0]  # type: ignore
+        if not isinstance(self._value, t):
             return Error(f"{repr(self)} cannot be set only to type {t}")
-        return Ok(self.value)  # type: ignore
+        return Ok(self._value)  # type: ignore
     
     @typing.overload
-    def detach(
-        self: "Variative[P, T]",
-    ) -> Ok[T]:
-        ...
-    
-    @typing.overload
-    def detach(
-        self: "Variative[T, typing.Unpack[Ps]]",
-    ) -> Ok["Variative[typing.Unpack[Ps]]"]:
+    def detach(self: Variative[P, T]) -> Ok[T]:
         ...
 
     @typing.overload
-    def detach(
-        self: "Variative[T, typing.Unpack[Ps]]", 
-    ) -> Result["Variative[typing.Unpack[Ps]]", str]:
+    def detach(self: Variative[P, *tuple[T, ...]]) -> Ok[Variative[T]]:
+        ...
+
+    @typing.overload
+    def detach(self: Variative[P, *tuple[T, ...]]) -> Result[Variative[T], str]:
         ...
     
-    def detach(  # type: ignore
-        self,
-    ):
-        """detaches head type. To make this customizable Python must implement intersection typing
+    def detach(self):
+        """Detaches head type. To make this customizable Python must implement intersection typing
         ```python
-        u: Variative[str, int]
+        v = Variative[str, int]
 
-        u = Variative("Hello")
-        u.detach() # Ok(Variative[str](value="Hello"))
+        v("Hello").detach() #> Error("Variative[str, int]('Hello') is of type <class 'str'>. Thus, head cannot be detached")
 
-        u = Variative(1)
-        u.detach() # Err
+        v(1).detach() #> Ok(Variative[int](1))
         ```
         """
+
         head, *tail = self.get_args()
-        if isinstance(self.value, head) and not isinstance(self.value, tuple(tail)):
-            return Error(f"{repr(self)} is of type {head}. thus, head cannot be detached")
+        if isinstance(self._value, head) and not isinstance(self._value, tuple(tail)):  # type: ignore
+            return Error(f"{repr(self)} is of type {head}. Thus, head cannot be detached")
         if len(self.get_args()) - 1 == 1:
-            return Ok(self.value)
-        return Ok(Variative[*self.get_args()[1:]](self.value))  # type: ignore
-    
+            return Ok(self._value)
+        return Ok(Variative[*self.get_args()[1:]](self._value))  # type: ignore
 
 
-__all__ = (
-    "Variative",
-)
+__all__ = ("Variative",)
