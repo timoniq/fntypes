bfae3a19994ab0c34fe6db9c954692c11baa5090
Georgy Pliev <howluwqz1@gmail.com>
2025-04-03 22:16:38 +0300

    refactor: python style 3.12

--- fntypes/co.py
+++ fntypes/co.py
@@ -1,6 +1,9 @@
 """Common Objects for *all* import"""
 
 from .error import UnwrapError
+from .lazy import Lazy
+from .lazy_coro import LazyCoro
+from .lazy_coro_result import LazyCoroResult
 from .option import Nothing, Option, Some
 from .result import RESULT_ERROR_LOGGER, Error, Ok, Result, Wrapped
 from .tools.unwrapping import unwrapping
@@ -10,6 +13,9 @@ __all__ = (
     "RESULT_ERROR_LOGGER",
     "Option",
     "Some",
+    "Lazy",
+    "LazyCoro",
+    "LazyCoroResult",
     "Nothing",
     "Result",
     "Ok",
--- fntypes/misc.py
+++ fntypes/misc.py
@@ -3,13 +3,11 @@ import typing
 from fntypes.result import Ok, Result
 
 
-def this[T](obj: T) -> T:
+def this[T](obj: T, /) -> T:
     return obj
 
 
-def either[T, Err](
-    result: Result[T, Err], or_: typing.Callable[[], Result[T, Err]]
-) -> Result[T, Err]:
+def either[T, Err](result: Result[T, Err], or_: typing.Callable[[], Result[T, Err]], /) -> Result[T, Err]:
     match result:
         case Ok(_):
             return result
@@ -17,7 +15,4 @@ def either[T, Err](
             return or_()
 
 
-__all__ = (
-    "this",
-    "either",
-)
+__all__ = ("this", "either")
